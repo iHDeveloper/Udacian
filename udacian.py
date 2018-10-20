23a9b2b5673e158e1227df061736f7d0441f2471
iHDeveloper <hamzadevelop@gmail.com>
2018-10-20 09:51:36 +0300

    Initial Commit

---
+++ udacian.py
@@ -0,0 +1,19 @@
+class Udacian: 
+    def __init__(self, name, city, enrollment, nanodegree, status):
+        self.name = name
+        self.city = city
+        self.enrollment = enrollment
+        self.nanodegree = nanodegree
+        self.status = status
+    def __str__(self):
+        return "{} is enrolled in {} studying {} in {} with {}, he/she is {}".format(
+            self.name,
+            self.city,
+            self.nanodegree,
+            self.enrollment[0],
+            self.enrollment[1],
+            self.status
+        )
+
+udacian = Udacian('Mohammed', 'Riyadh', [ 'Sat am', 'Ms. Lujuain' ], 'FSND', 'on track')
+print(udacian)
